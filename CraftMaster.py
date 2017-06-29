d445ce51f1024f078941d4179398c10e87868ad4
Hannah von Reth <vonreth@kde.org>
2017-06-29 09:51:06 +0200

    Move checkouts etc by default in the subfolder work

--- CraftMaster.py
+++ CraftMaster.py
@@ -7,43 +7,54 @@ import subprocess
 class CraftMaster(object):
     def __init__(self, configFile, commands):
         self.commands = commands
-        self._init()
         self._setConfig(configFile)
 
 
+    @property
+    def defaultWorkDir(self):
+        return os.path.join(os.getcwd(), "work")
 
-    def _init(self):
+
+
+    def _init(self, workDir):
         if not subprocess.getoutput("git config --global --get url.git://anongit.kde.org/.insteadof") == "kde:":
             subprocess.run(["git", "config", "--global", "url.git://anongit.kde.org/.insteadOf", "kde:"])
             subprocess.run(["git", "config", "--global", "url.ssh://git@git.kde.org/.pushInsteadOf", "kde:"])
             subprocess.run(["git", "config", "--global", "core.autocrlf", "false"])
             subprocess.run(["git", "config", "--system", "core.autocrlf", "false"])
-        craftClone = os.path.join(os.getcwd(), "craft-clone")
+        craftClone = os.path.join(workDir, "craft-clone")
         if not os.path.exists(craftClone):
             subprocess.run(["git", "clone", "kde:craft", craftClone])
+        else:
+            subprocess.run(["git", "pull"],  cwd=craftClone)
 
 
 
-    def _setRoots(self, craftRoots):
+    def _setRoots(self, workDir, craftRoots):
         self.craftRoots = {}
         for root in craftRoots:
-            craftRoot = os.path.join(os.getcwd(), root)
+            craftRoot = os.path.join(workDir, root)
             if not os.path.isdir(craftRoot):
                 os.makedirs(os.path.join(craftRoot, "etc"))
             if not os.path.isfile(os.path.join(craftRoot, "craft", "craftenv.ps1")):
-                subprocess.run(["cmd", "/C", "mklink", "/J", os.path.join(craftRoot, "craft"), os.path.join(os.getcwd(), "craft-clone")])
+                subprocess.run(["cmd", "/C", "mklink", "/J", os.path.join(craftRoot, "craft"), os.path.join(workDir, "craft-clone")])
             self.craftRoots[root] = craftRoot
 
     def _setConfig(self, configFile):
         parser = configparser.ConfigParser(interpolation=configparser.ExtendedInterpolation())
         parser.optionxform = lambda option: option
         parser.read(configFile)
+        if not "Root" in parser["Variables"]:
+            parser["Variables"]["Root"] = self.defaultWorkDir
+        workDir = parser["Variables"]["Root"]
         roots = [root for root in parser.sections() if not root in ["General", "GeneralSettings", "Variables"]]
         if not roots:
             print("Please specify at least one root category")
             exit(0)
 
-        self._setRoots(roots)
+        self._init(workDir)
+
+        self._setRoots(workDir, roots)
 
         if "GeneralSettings" in parser:
             self._setSetting(parser["GeneralSettings"].items())
